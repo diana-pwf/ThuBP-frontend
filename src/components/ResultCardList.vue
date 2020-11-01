eccdb3c9af66476ddf545887775503da3ea05388
Diana_pwf <Diana_pwf@163.com>
2020-11-01 11:19:58 +0800

    添加了自定义样式的展示搜索结果的卡片组件

--- src/components/ResultCardList.vue
+++ src/components/ResultCardList.vue
@@ -1,16 +1,31 @@
 <template>
-  <h1>这里是卡片列表</h1>
+ <SearchResultCard>
+     <img
+         alt="example"
+         src="background.png"
+         slot="card-img"
+     />
+     <h2 slot="card-title">Europe Street beat</h2>
+     <p slot="card-content">www.instagram.com</p>
+ </SearchResultCard>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import SearchResultCard from "./SearchResultCard.vue";
 
-@Component
+@Component({
+  components:{SearchResultCard}
+})
 export default class ResultCardList extends Vue {
 
 }
 </script>
 
 <style scoped>
-
+img {
+  max-width: 100%;
+  max-height: 100%;
+  margin: 0
+}
 </style>
---
+++ src/components/SearchResultCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-card class="card" hoverable="" style="width:1000px">
+    <div class="mycard">
+      <div class="card-img">
+        <slot name="card-img"></slot>
+      </div>
+      <div class="card-text">
+        <slot name="card-title"></slot>
+        <slot name="card-content"></slot>
+        <!--    <a-card-meta title="Europe Street beat">-->
+        <!--      <template slot="description">-->
+        <!--        www.instagram.com-->
+        <!--      </template>-->
+        <!--    </a-card-meta>-->
+      </div>
+    </div>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SearchResultCard extends Vue {
+
+}
+</script>
+
+<style scoped>
+.card {
+  height: 268px;
+}
+
+.mycard {
+  height: 220px;
+  display: grid;
+  grid-template-columns: 2fr 5fr;
+  grid-column-gap: 20px;
+}
+
+.card-img {
+  height: 220px;
+}
+
+.card-text {
+  text-align: left;
+}
+</style>
