90d69f71152c38650a394e691a8ac7a19416b9ec
Diana_pwf <Diana_pwf@163.com>
2020-11-01 11:48:05 +0800

    添加了卡片列表组件

--- src/components/ResultCardList.vue
+++ src/components/ResultCardList.vue
@@ -1,13 +1,18 @@
 <template>
- <SearchResultCard>
-     <img
-         alt="example"
-         src="background.png"
-         slot="card-img"
-     />
-     <h2 slot="card-title">Europe Street beat</h2>
-     <p slot="card-content">www.instagram.com</p>
- </SearchResultCard>
+  <ul id="result-card-list">
+    <li v-for="item in matchLists" :key="item.id">
+      <SearchResultCard>
+        <img
+            alt="example"
+            :src=item.imgSrc
+            slot="card-img"
+        />
+        <h2 slot="card-title">{{item.name}}</h2>
+        <p slot="card-content">{{item.content}}</p>
+      </SearchResultCard>
+    </li>
+  </ul>
+
 </template>
 
 <script lang="ts">
@@ -17,12 +22,30 @@ import SearchResultCard from "./SearchResultCard.vue";
 @Component({
   components:{SearchResultCard}
 })
-export default class ResultCardList extends Vue {
 
+export default class ResultCardList extends Vue {
+  matchCardInfo1 = {
+    id:1,
+    imgSrc:"background.png",
+    name:"Europe Street beat",
+    content:"www.instagram.com"
+  }
+
+  matchCardInfo2 = {
+    id:2,
+    imgSrc:"background.png",
+    name:"Europe Street beat",
+    content:"www.instagram.com"
+  }
+
+  matchLists = [this.matchCardInfo1, this.matchCardInfo2]
 }
 </script>
 
 <style scoped>
+ul, li {
+  list-style-type: none;
+}
 img {
   max-width: 100%;
   max-height: 100%;
