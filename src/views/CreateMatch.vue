5a41cab008004a473e11d5eb314d5d4f2768f524
diana-pwf <diana_pwf@163.com>
2020-11-24 00:06:12 +0800

    完成提交表单以创建赛事的接口通信

--- src/views/CreateMatch.vue
+++ src/views/CreateMatch.vue
@@ -5,7 +5,7 @@
     <a-form-model-item label="赛事名称" required="true">
       <a-input v-model="form.name" placeholder=""/>
     </a-form-model-item>
-    <a-form-model-item label="赛事地点" required="true">
+    <!--<a-form-model-item label="赛事地点" required="true">
       <a-select v-model="form.region" placeholder="请选择赛事地点">
         <a-select-option value="zicao">
           紫操
@@ -20,8 +20,8 @@
           北操
         </a-select-option>
       </a-select>
-    </a-form-model-item>
-    <a-form-model-item label="赛事开始时间" required="true">
+    </a-form-model-item>-->
+    <a-form-model-item label="赛事开始时间">
       <a-date-picker
           v-model="form.startDate"
           show-time
@@ -30,7 +30,7 @@
           style="width: 100%;"
       />
     </a-form-model-item>
-    <a-form-model-item label="赛事结束时间" required="true">
+    <a-form-model-item label="赛事结束时间">
       <a-date-picker
           v-model="form.endDate"
           show-time
@@ -40,10 +40,10 @@
       />
     </a-form-model-item>
     <a-form-model-item label="面向人群" required="true">
-      <a-input v-model="form.crowd" placeholder="请描述你服务的客户，内部客户直接 @姓名 / 工号"/>
+      <a-input v-model="form.targetGroup" placeholder="请描述你服务的客户，内部客户直接 @姓名 / 工号"/>
     </a-form-model-item>
     <a-form-model-item label="赛事类型" required="true">
-      <a-select v-model="form.type" placeholder="请选择赛事类型">
+      <a-select v-model="form.matchtypeId" placeholder="请选择赛事类型">
         <a-select-option value="basketball">
           篮球
         </a-select-option>
@@ -58,18 +58,21 @@
     <a-form-model-item label="赛事简介">
       <a-input v-model="form.description" placeholder="输入赛事描述" type="textarea" />
     </a-form-model-item>
-    <a-form-model-item label="允许自由报名" required="true">
-      <a-radio-group v-model="form.freeSignUp">
-        <a-radio value="1">
-          是
-        </a-radio>
-        <a-radio value="0">
-          否
-        </a-radio>
-      </a-radio-group>
+    <a-form-model-item label="赛事开放限制" required="true">
+      <a-radio-group :options="options" :default-value="0" v-model="publicRestriction" />
     </a-form-model-item>
-    <a-form-model-item label="允许比赛公开" required="true">
+    <!--<a-form-model-item label="允许赛事公开" required="true">
       <a-radio-group v-model="form.public">
+        <a-radio value="true">
+          是
+        </a-radio>
+        <a-radio value="false">
+          否
+        </a-radio>
+      </a-radio-group>
+    </a-form-model-item>
+    <a-form-model-item label="允许自由报名" required="true">
+      <a-radio-group v-model="form.publicSignUp">
         <a-radio value="1">
           是
         </a-radio>
@@ -77,7 +80,8 @@
           否
         </a-radio>
       </a-radio-group>
-    </a-form-model-item>
+    </a-form-model-item>-->
+
     <a-form-model-item label="上传赛事照片">
       <a-upload-dragger
           name="file"
@@ -97,7 +101,7 @@
       <a-button style="float:right">
         取消
       </a-button>
-      <a-button id="submit-button" type="primary" @click="onSubmit" style="float:right">
+      <a-button id="submit-button" type="primary" @click="createMatch" style="float:right">
         提交
       </a-button>
     </a-form-model-item>
@@ -120,17 +124,73 @@ import Navigation from "@/components/Navigation.vue";
 export default class CreateMatch extends Vue {
   labelCol = { span: 4 }
   wrapperCol = { span: 10 }
+  publicRestriction = 0
   form = {
     name: '',
-    region: '',
+    // region: '',
     startDate: undefined,
     endDate: undefined,
-    crowd: '',
-    type: '',
+    targetGroup: '',
+    matchtypeId: '',
     description: '',
-    freeSignUp: false,
-    public: false
+    publicSignUp: false,
+    publicShow: false
   }
+
+  options = [
+    { label: '赛事公开，允许自由报名', value: 0 },
+    { label: '赛事不公开，允许自由报名', value: 1 },
+    { label: '赛事不公开，不允许自由报名', value: 2 },
+  ];
+
+  async createMatch() {
+    if(this.publicRestriction === 0)
+    {
+      this.form.publicSignUp = true
+      this.form.publicShow = true
+    }
+    else if(this.publicRestriction === 1)
+    {
+      this.form.publicSignUp = true
+      this.form.publicShow = false
+    }
+    else
+    {
+      this.form.publicSignUp = false
+      this.form.publicShow = false
+    }
+
+    try {
+      axios.defaults.headers.common["Authorization"] = window.localStorage.getItem('jwt')
+      let response = await axios({
+        method: 'post',
+        url: '/api/v1/match',
+        data: {
+          "name": this.form.name,
+          //"startTime":this.form.startDate,
+          //"endTime":this.form.endDate,
+          "description": this.form.description,
+          "targetGroup": this.form.targetGroup,
+          "matchTypeId": this.form.matchtypeId,
+          //"publicShow": this.form.publicShow,
+          "publicSignUp": this.form.publicSignUp
+        }
+      })
+      // 对response做处理
+      if (response.status === 200) {
+        this.$message.success('create match success!')
+        this.$router.push('/home')
+        // 在此处弹出提示 注册成功
+      }
+      else
+      {
+        // 输出错误提示
+      }
+    } catch (e) {
+      this.$message.error(JSON.stringify(e.response.data.error))
+    }
+  }
+
 }
 </script>
 
